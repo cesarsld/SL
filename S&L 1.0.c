abc8bbecd165107c0c85ec8f977686a5cae79a13
cesarsld <jaimes.cesar88@gmail.com>
2014-02-25 23:18:17 +0000

    V6
    Added roll number function

--- S&L 1.0.c
+++ S&L 1.0.c
@@ -3,14 +3,12 @@
 #include <time.h>
 
 int main() {
-	FILE * TokenPosition:
-	TokenPosition=fopen("Tokenposition.txt",)
-	
+
 
     srand(time(NULL)); 
-	int c=2000000, avgames=0, p=0, min_turns=100, max_turns=1;
+	int c=2000000, avgames=0, p=0, min_turns=100, max_turns=1, h=0;
     int i=0, tp = 0, dv, turn=0, ttp=0;   //tp stands for the token position and dn the die value
-	int tokenposition[101]={0};
+	int tokenposition[101]={0}, rollnumber[600]={0};
     int l1 = 1, l2 = 4, l3 = 9, l4 = 21, l5 = 28, l6 = 36, l7 = 51, l8 = 71, l9 = 80;
     int s1 = 16, s2 = 47, s3 = 49, s4 = 56, s5 = 62, s6 = 64, s7 = 87, s8 = 93, s9 = 95, s10 = 98;
 
@@ -36,86 +34,86 @@ tp = tp - dv;}
 
       if(tp==l1){
           tp = tp + 37;}
-       else if (tp==l2) {
+       if (tp==l2) {
 	      tp = tp + 10;
 	
 }
-else if (tp==l3) {
+if (tp==l3) {
 	      tp = tp + 22;
 	
 }
-else if (tp==l4) {
+if (tp==l4) {
 	      tp = tp + 21;
 	
 }
-else if (tp==l5) {
+if (tp==l5) {
 	      tp = tp + 56;
 	
 }
-else if (tp==l6) {
+if (tp==l6) {
 	      tp = tp + 8;
 	
 }
-else if (tp==l7) {
+if (tp==l7) {
 	      tp = tp + 16;
 	
 }
-else if (tp==l8) {
+if (tp==l8) {
 	      tp = tp + 20;
 	
 }
-else if (tp==l9) {
+if (tp==l9) {
 	
 	tp = tp +20;
 	
-	      goto someline;
+	  
 	
 }
-else if (tp==s1) {
+if (tp==s1) {
 	      tp = tp - 10;
 	
 }
-else if (tp==s2) {
+if (tp==s2) {
 	      tp = tp - 21;
 	
 }
-else if (tp==s3) {
+if (tp==s3) {
 	      tp = tp - 38;
 	
 }
-else if (tp==s4) {
+if (tp==s4) {
 	      tp = tp - 3;
 	
 }
-else if (tp==s5) {
+if (tp==s5) {
 	      tp = tp - 43;
 	
 }
-else if (tp==s6) {
+if (tp==s6) {
 	      tp = tp - 4;
 	
 }
-else if (tp==s7) {
+if (tp==s7) {
 	      tp = tp - 63;
 	
 }
-else if (tp==s8) {
+if (tp==s8) {
 	      tp = tp - 20;
 	
 }
-else if (tp==s9) {
+if (tp==s9) {
 	      tp = tp - 20;
 	
 }
-else if (tp==s10) {
+if (tp==s10) {
 	      tp = tp - 20;
 	
-}
-else if (tp==100){
-	goto someline;
 }
 
-
+if (tp==100) {
+	
+	tokenposition[100]=tokenposition[100]+1;
+}
     }  
    
 
@@ -125,13 +123,19 @@ if(turn<min_turns){
    if(turn>max_turns){
 	max_turns=turn;
 }
+rollnumber[turn]=rollnumber[turn]+1;
 }
 avgames=avgames/c;
 
 printf("average number of rolls is %d\n", avgames);
 for (p=0; p<101;p++){
-	printf("%d = %d\n", p, tokenposition[p]);
-	
+	printf(" %d\n",  tokenposition[p]);
+		
 }
 printf("Minimum turns to finish the game is %d and maximum turns is %d", min_turns, max_turns);
+for (h=0; h<601;h++){
+		printf(" %d\n", rollnumber[h]);
+		}
+	
+
 }

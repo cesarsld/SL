c8eaeb18b5255ceebae21dab4129723108037d1d
cesarsld <jaimes.cesar88@gmail.com>
2014-02-27 13:37:46 +0000

    V8
    Simplified structure, reduced line number
    Fixed the incrementation bug on square 99
    few other blips and blops…

--- S&L 1.0.c
+++ S&L 1.0.c
@@ -1,89 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//int snakesandladders(){
-	
-//}
+
+int turn = 0, tokenposition[101] = {0};
+void snakesandladders() {
+     int i = 0, dv = 0, tp = 0, t[101] = {0};
+          int array[19][2]={
+               {1, 37},
+               {4,10},
+               {9,22},
+               {21,21},
+               {28,56},
+               {36,8},
+               {51,16},
+               {71,20},
+               {80,20},
+               {16,-10},
+               {47,-21},
+               {49,-38},
+               {56,-3},
+               {62,-43},
+               {64,-4},
+               {87,-63},
+               {93,-20},
+               {95,-20},
+               {98,-20}  
+          };
+     for (i=0;i<19;i++){
+          t[array[i][0]]=array[i][1];
+          }
+
+     while (tp<=99){    
+          if (turn>0){
+               tokenposition[tp]=tokenposition[tp]+1;
+          }
+          turn = turn+1;
+          dv = rand()%6+1;
+          tp = tp + dv;
+	      
+          if(tp>100){
+               tp = tp - dv;
+               tokenposition[tp]=tokenposition[tp]-1;
+          }
+          tp=tp+t[tp];
+
+          if (tp==100) {
+               tokenposition[100]=tokenposition[100]+1;
+          }
+     }
+	   
+}
 int main() {
-
-
-    srand(time(NULL)); 
-	int c=2000000, avgames=0, p=0, min_turns=100, max_turns=1, h=0, q=0, w=0;
-    int i=0, tp = 0, dv, turn=0, ttp=0;   //tp stands for the token position and dn the die value
-	int tokenposition[101]={0}, rollnumber[450]={0};
-    int t[101]={0};
-	int array[19][2]={
-	{1, 37},
-	{4,10},
-	{9,22},
-	{21,21},
-	{28,56},
-	{36,8},
-	{51,16},
-	{71,20},
-	{80,20},
-	{16,-10},
-	{47,-21},
-	{49,-38},
-	{56,-3},
-	{62,-43},
-	{64,-4},
-	{87,-63},
-	{93,-20},
-	{95,-20},
-	{98,-20}   };
-for (w=0;w<19;w++){
-		t[array[w][0]]=array[w][1];
-	
-	}
-	
-	for(i=0; i<c;i++){
-	tp=0;
-	turn=0;
-    while (tp<=99){    
-	if (turn>0){
-		tokenposition[tp]=tokenposition[tp]+1;
-	}
-	turn = turn+1;
-       dv = rand()%6+1;
-
-       tp = tp + dv;
-
-       ttp = tp - 100;
-
-
-      
-if(tp>100){
-tp = tp - dv;}
-
-tp=tp+t[tp];
-
-
-if (tp==100) {
-	
-	tokenposition[100]=tokenposition[100]+1;
-}
-    }  
-   
-
-avgames= avgames+turn;
-if(turn<min_turns){
-    min_turns=turn;}
-   if(turn>max_turns){
-	max_turns=turn;
-}
-rollnumber[turn]=rollnumber[turn]+1;
-}
-avgames=avgames/c;
+     srand(time(NULL)); 
+     int c=2000000,h=0,p=0 ,  avgames=0;
+     int rollnumber[450]={0}, j=0;
+		
+     for(j=0; j<c;j++){
+          turn=0;
+          snakesandladders();
+          avgames= avgames+turn;
+          rollnumber[turn]=rollnumber[turn]+1;
+     }
+          avgames=avgames/c;
 
 printf("average number of rolls is %d\n", avgames);
-for (p=0; p<101;p++){
-	printf(" yop %d %d\n", p, tokenposition[p]);
-		
-}
-printf("Minimum turns to finish the game is %d and maximum turns is %d\n", min_turns, max_turns);
-for (h=1; h<451;h++){
-		printf(" %d\n", rollnumber[h]);
-		}
+     for (p=0; p<101;p++){
+	     printf("%d %d\n", p, tokenposition[p]);
+          }
+
+     for (h=1; h<451;h++){
+          printf(" %d\n", rollnumber[h]);
+          }
 
 }
